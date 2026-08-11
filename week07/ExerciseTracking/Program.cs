f67070db69edcdde949e4d1bdeff0ee94c72e15b
Edi Carlos <edi_csilva@hotmail.com>
2026-08-11 14:09:58 -0300

    W07 Assignment: Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("11 Ago 2026", 40, 4.0));
+        activities.Add(new Cycling("11 Ago 2026", 40, 12.0));
+        activities.Add(new Swimming("11 Ago 2026", 35, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
